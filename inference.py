44a8a02b3b847b7e5c02e446ca9b30daa8062960
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-13 15:28:07 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@ from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import openai
 
-openai.api.key = ""
+openai.api.key = "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"
 all_outputs = []
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -20,7 +20,7 @@ tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 # Load the Lora model
 model = PeftModel.from_pretrained(model, peft_model_id)
 
-
+#Generate Outputs Function
 def get_outputs(list1):
   
   for item in list:
@@ -31,15 +31,35 @@ def get_outputs(list1):
 
     final_output = tokenizer.decode(output_tokens[0], skip_special_tokens=True))
     all_outputs.append(final_output)
+    return all_outputs
     
-    
-def generate_questions_list():
-  content = "Generate random questions which can be used to evaluate the performance of a Large Language model. Please number them"
-  response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = [{"role": "user", "content": content}])
-  
-  message = response['choices'][0]['message']['content']
-  
-  return message
+#Generate Questions Function    
+def generate_questions_list(num_questions):
+  openai.api_key = "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"
+  content = f"Can you generate {num_questions} questions which can help the performance of LLMs?"
+  response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = [{"role":"user", "content": content}])
 
+
+  message = response['choices'][0]["message"]["content"]
+
+  data = str(message)
+  print(data)
+  import re
+  # Assuming 'data' is your string containing all the questions
+  questions = re.split(r'\d+\.', data)
+  questions = [question.strip() for question in questions if question.strip()]
+  all_questions = all_questions + questions
+  questions = []
+
+  
+  
+  def main():
+    all_questions = generate_questions_list(100)
+    all_outputs = get_outputs(all_questions)
+    final_dict = {"input" : all_questions , "output" : all_outputs)
+    final_df = pd.DataFrame(final_dict)
+ 
+main()                  
+    
 
     
